ae2f5ebe89d2ecf4eea4ec9ecd28cde4e32e3122
Augusto Fadanelli de Sousa <32231958+Augusto-Fadanelli@users.noreply.github.com>
2019-08-27 17:58:52 -0300

    Update main.cpp
    Falta inserir por posição

--- main.cpp
+++ main.cpp
@@ -23,6 +23,11 @@ typedef struct no{
 }no;
 
 int menu();
+
+int inserir(int, no **, no **);
+void excluir(int *, no *, no **, no **);
+void listar(no *, no **);
+
 void clear();
 void pause();
 
@@ -35,7 +40,7 @@ int main()
 	lista = NULL; //O primeiro valor da lista aponta para NULL
 	no *novo;
 
-	int num;
+	int cont = 0;
    	int flag = 0; //Flag para sair do loop
 
    	no *aux;//usado em excluir e pesquisar
@@ -44,69 +49,24 @@ int main()
 
         switch(menu()){
         case 1:
-		//inserir();
+            cont = inserir(cont, &novo, &lista);
 
-		cout << "Digite um Número inteiro para inserir na lista lista: ";
-		cin >> num;
-		cout << endl;
-
-		novo = (no *)malloc(sizeof(no)); //O ponteiro 'novo' recebe o local indicado por malloc(); do tamanho definido por sizeof();
-
-		if (novo == NULL){ //Impede que o programa execute caso o malloc não consiga encontrar memória livre
-                	cout << "Memória insuficiente!\n Tente fechar alguns programas ou reiniciar a máquina." << endl;
-                	pause();
-               		if(lista == NULL){ // se novo e lista == NULL então nada foi adicionado na lista e o programa pode ser fechado
-                    		exit(1);
-                	} // se a lista possuir elementos o programa não fecha, mesmo com o malloc não conseguindo encontrar espaços de memória livres
-            	}else{
-                	novo->valor=num;
-                	novo->prox=lista; //Lista inicialmente vale NULL, depois de ser inserido algum valor 'lista' vai ser igual ao ptr anterior
-                	lista=novo;
-            	}
         break;
         case 2:
-		//excluir();
-            int ind;
-            no *morta;
-            aux = novo;
-            cout << "Digite o índice do valor a ser excluido: ";
-            cin >> ind;
-
-            for(int i=0; i<ind-2; i++){
-                aux = aux->prox;
-            }
-            morta = aux->prox;
-            aux->prox = morta->prox;
-
-
+            excluir(&cont, novo, &novo, &lista);
 
         break;
         case 3:
-		//listar();
+		listar(novo, &lista);
 
-            if(lista == NULL){
-                cout << "Lista vazia!" << endl;
-                pause();
-            }else{
-                cout << endl;
-                cout << endl;
-                aux = novo;
-                int cont = 0;
-                	do{
-                        cout << cont+1 << "º "<< aux->valor << " | ";
-                		aux = aux->prox; //O ptr 'aux' agora vai apontar para o proximo valor
-                		cont++;
-                	}while(aux != NULL);
-                	cout << endl;
-                    cout << endl;
-                    cout << "A lista possui " << cont <<  " valores." << endl;
-                pause();
-            }
+
 
         break;
         case 4:
         //limpar();
             lista = NULL; //perde-se o local do ultimo valor informado
+            novo = NULL;
+            cont = 0;
         break;
         case 5:
         //Sair
@@ -122,6 +82,81 @@ int main()
     }while(flag != 1);
 
     return 0;
+}
+
+int inserir(int cont, no **novo, no **lista){
+
+    int num;
+
+    cout << "Digite um Número inteiro para inserir na lista lista: ";
+    cin >> num;
+    cout << endl;
+
+    *novo = (no *)malloc(sizeof(no)); //O ponteiro 'novo' recebe o local indicado por malloc(); do tamanho definido por sizeof();
+
+    if (*novo == NULL){ //Impede que o programa execute caso o malloc não consiga encontrar memória livre
+        cout << "Memória insuficiente!\n Tente fechar alguns programas ou reiniciar a máquina." << endl;
+        pause();
+        if(*lista == NULL){ // se novo e lista == NULL então nada foi adicionado na lista e o programa pode ser fechado
+            exit(1);
+        } // se a lista possuir elementos o programa não fecha, mesmo com o malloc não conseguindo encontrar espaços de memória livres
+    }else{
+        (*novo)->valor=num;
+        (*novo)->prox=*lista; //Lista inicialmente vale NULL, depois de ser inserido algum valor 'lista' vai ser igual ao ptr anterior
+        *lista=*novo;
+        cont++;
+    }
+
+    return cont;
+}
+
+void excluir(int *cont, no *aux, no **novo, no **lista){
+
+    int ind;
+    no *morta;
+    cout << "Digite o índice do valor a ser excluido: ";
+    cin >> ind;
+
+    if(ind > 1 && ind <= *cont){ //Impede que um valor de um indice que não exista seja excluido (O que poderia causar uma falha de segmentação "Crashar")
+        for(int i=0; i<ind-2; i++){
+            aux = aux->prox;
+        }
+        morta = aux->prox;
+        aux->prox = morta->prox;
+        cont--;
+    }else{
+        if(ind == 1 && *cont != 0){ //Impede a execução caso não exista indice
+            *novo = aux->prox;
+            *lista = *novo;
+            cont--;
+        }else{
+            cout << "Índice inválido!" << endl;
+            pause();
+        }
+    }
+
+}
+
+void listar(no *aux, no **lista){
+
+    if(*lista == NULL){
+        cout << "Lista vazia!" << endl;
+        pause();
+    }else{
+        cout << endl;
+        cout << endl;
+        int cont = 0;
+        do{
+            cout << cont+1 << "º "<< aux->valor << " | ";
+            aux = aux->prox; //O ptr 'aux' agora vai apontar para o proximo valor
+            cont++;
+        }while(aux != NULL);
+        cout << endl;
+        cout << endl;
+        cout << "A lista possui " << cont <<  " valores." << endl;
+        pause();
+    }
+
 }
 
 int menu(){
